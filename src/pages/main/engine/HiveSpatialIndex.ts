d9ed244e26c89bf98632f01c968cd6cf5d447c6a
choiboa <musamea99@gmail.com>
2026-01-26 20:23:52 +0900

    🚨 Fix: NDC 기반 가상 스크롤 + 프리패칭 방식 변경

--- src/pages/main/engine/HiveSpatialIndex.ts
+++ src/pages/main/engine/HiveSpatialIndex.ts
@@ -2,6 +2,7 @@ export class HiveSpatialIndex {
   private items: {
     index: number;
     x: number;
+    y: number;
     z: number;
     modelPath: string;
   }[];
@@ -12,9 +13,12 @@ export class HiveSpatialIndex {
     this.items = positionedRooms.map(({ room, position }, index) => ({
       index,
       x: position[0],
+      y: position[1],
       z: position[2],
       modelPath: room.modelPath ?? '',
     }));
+
+    this.items.sort((a, b) => a.x - b.x);
   }
 
   getAll() {
